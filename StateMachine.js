28c1269e77c5a9dab0e04eec6e2e93ce6581dbf1
Christian Lewis <ctjl96@gmail.com>
2020-08-06 13:44:15 -0500

    Committing changes before push to remote.

--- StateMachine.js
+++ StateMachine.js
@@ -1,29 +1,26 @@
 /**
  * @license MIT
- * @fileoverview Source code for the StateMachine
- * class.
+ * @fileoverview
+ * Source code for the StateMachine class.
  */
 
 /**
- * A key-map Object containing all state
- * information.
+ * A key-map Object containing all state information.
  *
  * @typedef {Object} State
  */
 let State;
 
 /**
- * A callback which accepts a `State` as an
- * argument.
+ * A callback which accepts a `State` as an argument.
  *
  * @typedef {function(State)} StateTransition
  */
 let StateTransition;
 
 /**
- * A callback which accepts a `State` as an
- * argument and returns a `Boolean` instructing
- * whether or not to call `update` again.
+ * A callback which accepts a `State` as an argument and returns a `Boolean`
+ * instructing whether or not to call `update` again.
  *
  * @typedef {function(State)} WhileCondition
  */
@@ -48,7 +45,7 @@ class StateMachine {
     /**
      * Load a state.
      *
-     * @param {State} state 
+     * @param {State} state
      * Value to load into `state`.
      */
     set(state) {
@@ -57,68 +54,47 @@ class StateMachine {
     }
 
     /**
-     * Update this machine's state by calling
-     * `StateTransition` while `WhileCondition` is
-     * `true`.
+     * Update this machine's state by calling `StateTransition` while
+     * `WhileCondition` is `true`.
      *
-     * @param {StateTransition} stateTransition 
-     * A callback which accepts a `State` as an
-     * argument. Inside the callback, `this`
-     * refers to this Widget.
+     * @param {StateTransition} stateTransition
+     * A callback which accepts a `State` as an argument. Inside the callback,
+     * `this` refers to this Widget.
      *
-     * @param {WhileCondition} whileCondition 
-     * A `StateTransition`-like callback. Returns
-     * `true` or `false`, indicating whether or
-     * not to call `update` again.
+     * @param {WhileCondition?} whileCondition 
+     * A `StateTransition`-like callback. Returns `true` or `false`, indicating
+     * whether or not to call `update` again.
      *
-     * @param {Number} maxCalls 
-     * The maximum number of times to trigger a
-     * state update. Defaults to `Infinity`.
+     * @param {Number?} maxCalls 
+     * The maximum number of times to trigger a state update. Defaults to
+     * `Infinity`.
      */
-    transition(
+    async transition(
         stateTransition,
         whileCondition,
         maxCalls = Infinity) {
 
         let i = 0;
 
-        if (
-            whileCondition &&
-            whileCondition(this.state) &&
-            i++ < maxCalls
-        ) {
-            stateTransition(this.state);
-            
-            requestAnimationFrame(
-                () => {
-                    this.transition(
-                        stateTransition, 
-                        whileCondition, 
-                        maxCalls
-                    );
-                });
-
-            return this.render();
-        }
+        while (
+            /** Run at least once. */
+            i++ === 0 ||
+            /** If < max and a WHILE condition is specified, compute it. */
+            (i < maxCalls && whileCondition && whileCondition(this.state))
+        ) await stateTransition(this.state);
 
         return this;
     }
 
     /**
-     * Update this machine's state. Analogous to
-     * `setState` in traditional nomenclature.
+     * Update this machine's state. Analogous to `setState` in traditional
+     * nomenclature.
      *
-     * @param {StateTransition} stateTransition
-     * A callback which accepts a `State` as an
-     * argument.
+     * @param {StateTransition} stateTransition 
+     * A callback which accepts a `State` as an argument.
      */
-    update(stateTransition) {
-        stateTransition(this.state);
-        return this.render();
-    }
-    
-    render() {
-        document.body.textContent = this.state.total;
+    async update(stateTransition) {
+        await stateTransition(this.state);
         return this;
     }
 }
---
+++ test/transitionTests.js
@@ -0,0 +1,29 @@
+import test from 'ava';
+import fs from 'fs';
+import { runInThisContext } from 'vm';
+
+global.requestAnimationFrame = (fn) => setTimeout(fn, 0);
+runInThisContext(fs.readFileSync('StateMachine.js'));
+
+test('1 -> 10', async (t) => {
+  const machine = new StateMachine({
+    a: 1
+  })
+  
+  await machine.transition(
+    state => state.a++,
+    state => state.a < 10
+  );
+
+  t.is(machine.state.a, 10);
+});
+
+test('"red" -> "blue"', async (t) => {
+  const machine = new StateMachine({
+    color: 'red'
+  });
+
+  await machine.transition(state => state.color = 'blue');
+
+  t.is(machine.state.color, 'blue');
+})
